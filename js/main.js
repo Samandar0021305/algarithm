5044a37c268f04e9a856462bb8bea21c68625c81
Samandar0021305 <samandar021325@gmail.com>
2022-10-03 17:39:33 +0500

    init

--- js/main.js
+++ js/main.js
@@ -1,17 +1,35 @@
 // 10-masala
-function oddishOrEvenish(number){
-    let res = number.toString().split("");
-      let summa = 0 
-      for(let item of res){
-        summa +=item;
-      }
+// function oddishOrEvenish(number){
+//     let res = number.toString().split("");
+//       let summa = 0 
+//       for(let item of res){
+//         summa +=item;
+//       }
     
-      if(summa %2 ==0){
-        console.log("Oddish");
-      }else if(summa %2 ==1){
-        console.log("Evish");
-      }
+//       if(summa %2 ==0){
+//         console.log("Oddish");
+//       }else if(summa %2 ==1){
+//         console.log("Evish");
+//       }
+//     }
+    
+//     let number = 50;
+//     oddishOrEvenish(number)
+
+
+// 11-masala
+
+function numInStr(arry = []){
+    let newArry = []
+ arry.filter((element)=>{
+   element.split("").filter((index) =>{
+    if(index / 1 == index){
+      newArry.push(element)
     }
-    
-    let number = 50;
-    oddishOrEvenish(number)
+   });
+ })
+ return newArry     
+}
+
+let arry = ["la","a","2b","b","9e"]
+console.log(numInStr(arry));
