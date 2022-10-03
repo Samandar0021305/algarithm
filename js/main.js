6a06065d7fa65c1d9e007882cb76a060d1bacaeb
Samandar0021305 <samandar021325@gmail.com>
2022-10-03 18:05:49 +0500

    init

--- js/main.js
+++ js/main.js
@@ -19,17 +19,68 @@
 
 // 11-masala
 
-function numInStr(arry = []){
-    let newArry = []
- arry.filter((element)=>{
-   element.split("").filter((index) =>{
-    if(index / 1 == index){
-      newArry.push(element)
-    }
-   });
- })
- return newArry     
+// function numInStr(arry = []){
+//     let newArry = []
+//  arry.filter((element)=>{
+//    element.split("").filter((index) =>{
+//     if(index / 1 == index){
+//       newArry.push(element)
+//     }
+//    });
+//  })
+//  return newArry     
+// }
+
+// let arry = ["la","a","2b","b","9e"]
+// console.log(numInStr(arry));
+
+
+// 23-masala
+
+
+
+function balanced(str = " "){
+    const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+    let one = [str.split("")[0],str.split("")[1]]
+    let two = [str.split("")[str.split("").length - 2],str.split("")[str.split("").length - 1]]
+   
+    let oneNum = 0;
+    let oneNum1 = 0
+      for(let i = 1; i < alphabet.length ; i++){
+        if(alphabet[i].toLowerCase() == one[0] ){
+            oneNum = i;
+        }else if(alphabet[i].toLowerCase() == one[1]){
+            oneNum1 = i;
+        }
+      }
+
+
+      let oneSumma = oneNum + oneNum1;
+
+
+      let twoNum = 0;
+      let twoNum1 = 0
+      for(let i = 1; i < alphabet.length ; i++){
+        if(alphabet[i].toLowerCase() == two[0] ){
+            twoNum = i;
+        }else if(alphabet[i].toLowerCase() == two[1]){
+            twoNum1 = i;
+        }
+      }
+     
+
+      let summTwo =  twoNum + twoNum1;
+
+      if(summTwo ==oneSumma ){
+         return true
+      }else{
+        return false
+      }
+
+    
+
 }
 
-let arry = ["la","a","2b","b","9e"]
-console.log(numInStr(arry));
+let str = "zips"
+console.log(balanced(str));
+
