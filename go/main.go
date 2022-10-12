684880a39de08a8cad03217d2fee1411f2963bca
Samandar0021305 <samandar021325@gmail.com>
2022-10-12 10:34:21 +0500

    init

--- go/main.go
+++ go/main.go
@@ -1,112 +1,160 @@
-package crawler
+// package crawler
 
-import (
-	"bufio"
-	"bytes"
-	"errors"
-	"fmt"
-	"io/ioutil"
-	"os"
-	"path/filepath"
-	"strings"
+// import (
+// 	"bufio"
+// 	"bytes"
+// 	"errors"
+// 	"fmt"
+// 	"io/ioutil"
+// 	"os"
+// 	"path/filepath"
+// 	"strings"
 
-	"github.com/pilebones/go-udev/netlink"
-)
+// 	"github.com/pilebones/go-udev/netlink"
+// )
 
-const (
-	BASE_DEVPATH = "/sys/devices"
-)
+// const (
+// 	BASE_DEVPATH = "/sys/devices"
+// )
 
-type Device struct {
-	KObj string
-	Env  map[string]string
-}
+// type Device struct {
+// 	KObj string
+// 	Env  map[string]string
+// }
 
-// ExistingDevices return all plugged devices matched by the matcher
-// All uevent files inside /sys/devices is crawled to match right env values
-func ExistingDevices(queue chan Device, errs chan error, matcher netlink.Matcher) chan struct{} {
-	quit := make(chan struct{}, 1)
+// // ExistingDevices return all plugged devices matched by the matcher
+// // All uevent files inside /sys/devices is crawled to match right env values
+// func ExistingDevices(queue chan Device, errs chan error, matcher netlink.Matcher) chan struct{} {
+// 	quit := make(chan struct{}, 1)
 
-	if matcher != nil {
-		if err := matcher.Compile(); err != nil {
-			errs <- fmt.Errorf("Wrong matcher, err: %w", err)
-			quit <- struct{}{}
-			close(queue)
-			return quit
-		}
-	}
+// 	if matcher != nil {
+// 		if err := matcher.Compile(); err != nil {
+// 			errs <- fmt.Errorf("Wrong matcher, err: %w", err)
+// 			quit <- struct{}{}
+// 			close(queue)
+// 			return quit
+// 		}
+// 	}
 
-	go func() {
-		err := filepath.Walk(BASE_DEVPATH, func(path string, info os.FileInfo, err error) error {
-			select {
-			case <-quit:
-				return errors.New("abort signal receive")
-			default:
-				if err != nil {
-					return err
-				}
+// 	go func() {
+// 		err := filepath.Walk(BASE_DEVPATH, func(path string, info os.FileInfo, err error) error {
+// 			select {
+// 			case <-quit:
+// 				return errors.New("abort signal receive")
+// 			default:
+// 				if err != nil {
+// 					return err
+// 				}
 
-				if info.IsDir() || info.Name() != "uevent" {
-					return nil
-				}
+// 				if info.IsDir() || info.Name() != "uevent" {
+// 					return nil
+// 				}
 
-				env, err := getEventFromUEventFile(path)
-				if err != nil {
-					return err
-				}
+// 				env, err := getEventFromUEventFile(path)
+// 				if err != nil {
+// 					return err
+// 				}
 
-				kObj := filepath.Dir(path)
+// 				kObj := filepath.Dir(path)
 
-				// Append to env subsystem if existing
-				if link, err := os.Readlink(kObj + "/subsystem"); err == nil {
-					env["SUBSYSTEM"] = filepath.Base(link)
-				}
+// 				// Append to env subsystem if existing
+// 				if link, err := os.Readlink(kObj + "/subsystem"); err == nil {
+// 					env["SUBSYSTEM"] = filepath.Base(link)
+// 				}
 
-				if matcher == nil || matcher.EvaluateEnv(env) {
-					queue <- Device{
-						KObj: kObj,
-						Env:  env,
-					}
-				}
-				return nil
-			}
-		})
+// 				if matcher == nil || matcher.EvaluateEnv(env) {
+// 					queue <- Device{
+// 						KObj: kObj,
+// 						Env:  env,
+// 					}
+// 				}
+// 				return nil
+// 			}
+// 		})
 
-		if err != nil {
-			errs <- err
-		}
+// 		if err != nil {
+// 			errs <- err
+// 		}
 
-		close(queue)
-	}()
-	return quit
-}
+// 		close(queue)
+// 	}()
+// 	return quit
+// }
 
-// getEventFromUEventFile return all env var define in file
-// syntax: name=value for each line
-// Fonction use for /sys/.../uevent files
-func getEventFromUEventFile(path string) (map[string]string, error) {
-	f, err := os.Open(path)
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
+// // getEventFromUEventFile return all env var define in file
+// // syntax: name=value for each line
+// // Fonction use for /sys/.../uevent files
+// func getEventFromUEventFile(path string) (map[string]string, error) {
+// 	f, err := os.Open(path)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	defer f.Close()
 
-	data, err := ioutil.ReadAll(f)
-	if err != nil {
-		return nil, err
-	}
-	return getEventFromUEventData(data), nil
-}
+// 	data, err := ioutil.ReadAll(f)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	return getEventFromUEventData(data), nil
+// }
 
-func getEventFromUEventData(data []byte) map[string]string {
-	rv := make(map[string]string)
-	buf := bufio.NewScanner(bytes.NewBuffer(data))
-	for buf.Scan() {
-		field := strings.SplitN(buf.Text(), "=", 2)
-		if len(field) != 2 {
-			return rv // TODO: return error ?
-		}
-		rv[field[0]] = field[1]
-	}
-	return rv
-}
+// func getEventFromUEventData(data []byte) map[string]string {
+// 	rv := make(map[string]string)
+// 	buf := bufio.NewScanner(bytes.NewBuffer(data))
+// 	for buf.Scan() {
+// 		field := strings.SplitN(buf.Text(), "=", 2)
+// 		if len(field) != 2 {
+// 			return rv // TODO: return error ?
+// 		}
+// 		rv[field[0]] = field[1]
+// 	}
+// 	return rv
+// }
+
+// package main
+
+// import (
+// 	"fmt"
+// 	"reflect"
+// )
+
+// func main(){
+// 	fmt.Println("hello playground")
+// 	// fmt.Println(math.Floor(2.34))
+// 	fmt.Println("A")
+// 	fmt.Println(reflect.TypeOf(1))
+// 	fmt.Println(reflect.TypeOf(1.5))
+// }
+
+
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+
+
+// // func main(
+// // var d int
+
+// // fmt.Scan(&d)
+
+	
+// // )
+
+
+// // package main
+
+// // import (
+// // 	"fmt"
+// // )
+
+// // func main()
+// // var x int 
+// // var y int
+// // fmt.Scanln(&x)
+// // fmt.Scan(&y)
+// // sum := x + y
+// // fmt.Println(sum)
+// // }
--- js/main.js
+++ js/main.js
@@ -1,20 +1,22 @@
 // 10-masala
+
 // function oddishOrEvenish(number){
 //     let res = number.toString().split("");
 //       let summa = 0 
 //       for(let item of res){
-//         summa +=item;
+//         summa += (item - 0);
 //       }
     
-//       if(summa %2 ==0){
+//       if(summa %2 == 0){
 //         console.log("Oddish");
-//       }else if(summa %2 ==1){
+    
+//       }else if(summa % 2 ==1){
 //         console.log("Evish");
 //       }
 //     }
     
-//     let number = 50;
-//     oddishOrEvenish(number)
+//     let number = 300;
+//     oddishOrEvenish(number);
 
 
 // 11-masala
@@ -31,7 +33,7 @@
 //  return newArry     
 // }
 
-// let arry = ["la","a","2b","b","9e"]
+// let arry = ["la","a","2b","b","9e","2a"]
 // console.log(numInStr(arry));
 
 
@@ -39,48 +41,201 @@
 
 
 
-function balanced(str = " "){
-    const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-    let one = [str.split("")[0],str.split("")[1]]
-    let two = [str.split("")[str.split("").length - 2],str.split("")[str.split("").length - 1]]
-   
-    let oneNum = 0;
-    let oneNum1 = 0
-      for(let i = 1; i < alphabet.length ; i++){
-        if(alphabet[i].toLowerCase() == one[0] ){
-            oneNum = i;
-        }else if(alphabet[i].toLowerCase() == one[1]){
-            oneNum1 = i;
-        }
-      }
+// function balanced(str = " "){
+//     const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+//     let one = [str.split("")[0],str.split("")[1]]
+//     let two = [str.split("")[str.split("").length - 2],str.split("")[str.split("").length - 1]]
+
+//     let oneNum = 0;
+//     let oneNum1 = 0
+//       for(let i = 1; i < alphabet.length ; i++){
+//         if(alphabet[i].toLowerCase() == one[0] ){
+//             oneNum = i;
+//         }else if(alphabet[i].toLowerCase() == one[1]){
+//             oneNum1 = i;
+//         }
+//       }
 
 
-      let oneSumma = oneNum + oneNum1;
+//       let oneSumma = oneNum + oneNum1;
 
 
-      let twoNum = 0;
-      let twoNum1 = 0
-      for(let i = 1; i < alphabet.length ; i++){
-        if(alphabet[i].toLowerCase() == two[0] ){
-            twoNum = i;
-        }else if(alphabet[i].toLowerCase() == two[1]){
-            twoNum1 = i;
-        }
-      }
-     
+//       let twoNum = 0;
+//       let twoNum1 = 0
+//       for(let i = 1; i < alphabet.length ; i++){
+//         if(alphabet[i].toLowerCase() == two[0] ){
+//             twoNum = i;
+//         }else if(alphabet[i].toLowerCase() == two[1]){
+//             twoNum1 = i;
+//         }
+//       }
+//       let summTwo =  twoNum + twoNum1;
+//       if(summTwo ==oneSumma ){
+//          return true
+//       }else{
+//         return false
+//       }
+// }
 
-      let summTwo =  twoNum + twoNum1;
+// let str = "BZYA"
+// console.log(balanced(str));
 
-      if(summTwo ==oneSumma ){
-         return true
-      }else{
-        return false
-      }
 
+
+// function foo(){
+//     console.log(a);
+//     console.log(b);
+//     var a = 5;
+//     let b = 6
+// }
+
+// foo()
+
+
+
+
+
+// ixtiyoriy son berilgan sonlarning raqamlar yig'indisini 
+// juft toqligini aniqlaydigan dastur
+
+
+
+// function numbers(num){
+//     let arry = String(num).split("")
+//     let summa = 0;
+
+//     for(let item of arry){
+//      summa += (item-0)
+//     }
+
+//   if(summa % 2 == 0){
+//     return "Juft"
+//   }else if(summa % 2 == 1){
+//     return "Toq"
+//   }
+// }
+// console.log(numbers(432));
+
+
+
+
+
+
+// 1-masala
+// ixtiyoriy son berilgan sonlarning raqamlar yig'indisini 
+// juft toqligini aniqlaydigan dastur
+
+// 2-masala
+// massiv elementlari orasidan son qatnashganlarini 
+// yangi arrayga o'tkazish
+
+// 3-masala
+// ixtiyoriy so'z berilgan so'zning boshidagi
+// 2 ta va oxiridagi 2 ta harfni alifbodagi o'rni
+// raqamlar yig'indisi teng ekanligi haqida funksya
+
+
+// function numbers(num){
+//     let arry = String(num).split("");
+//     let summa = 0;
     
+//     for(let item of arry){
+//         summa += (item - 0)
+//     }
+
+//     if(summa % 2 ==0){
+//         return "Juft"
+//     }else if(summa % 2 == 1){
+//         return "Toq"
+//     }
+// }
+// console.log(numbers(131));
+
+
+
+// function numInStr(arry =[]){
+//     let newArry = [];
+   
+//    arry.filter((element)=>{
+//     element.split("").filter((index)=>{
+//         if(index / 1 == index){
+//               newArry.push(element) 
+//         }
+//     })
+//    })
+//     return newArry;
+// }
+
+// let arry = ["2b","a","la","3b","4k","9a"];
+// console.log(numInStr(arry));
+
+
+
+
+// function balanced(str=""){
+//     const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+//     let arry = str.split("")
+//     let one = [arry[0],arry[1]];
+//     let two = [arry[arry.length - 2], arry[arry.length - 1]];
+    
+//     let oneNum = 0;
+//     let oneNum1 = 0;
+//     for(let i = 0; i < alphabet.length ; i++){
+//         if(alphabet[i].toLowerCase() == one[0]){
+//             oneNum = i;
+//         }else if(alphabet[i].toLowerCase() == one[1]){
+//             oneNum1 = i; 
+//         }
+//     }
+
+//     let twoNum = 0;
+//     let twoNum1 = 0;
+
+//     for(let i = 0; i < alphabet.length ; i++){
+//         if(alphabet[i].toLowerCase() == two[0]){
+//             twoNum = i;
+//         }else if(alphabet[i].toLowerCase() == two[1]){
+//             twoNum1 = i; 
+//         }
+//     }
+
+//     let summaOneNum = oneNum + oneNum1;
+//     let summaTwoNum = twoNum + twoNum1;
+
+//     if(summaOneNum == summaTwoNum){
+//         return true
+//     }else if(summaOneNum != summaTwoNum){
+//         return false
+//     }
+
+// }
+
+// let str = "welcome";
+// console.log(balanced(str));
+
+
+function foo(arry = []){
+let num = [];
+ 
+ arry.filter((element)=>{
+    element.toString().split("").filter((index)=>{
+        if(Number(index) == 7){
+            num.push(element) 
+        }else if(Number(index) != 7){
+            return "None of the items contain 7 within  them."
+        }
+    })
+ })
+
+ for(let item of num){
+    if(item == 7){
+     return "Boom!"
+    }else{
+     return `${item} contins the number seven`
+    }
+ }
 
 }
 
-let str = "zips"
-console.log(balanced(str));
-
+let arry = [2,55,60,7,86]
+console.log(foo(arry));
